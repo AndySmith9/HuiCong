1b55b0e57a7d2be69bceb68663647aed375fe9e0
Andy <Andy@qq.com>
2024-12-15 17:34:29 +0800

    PInventory.py优化完成

--- pythonProject1/HH_s_template/PInventory.py
+++ pythonProject1/HH_s_template/PInventory.py
@@ -2,17 +2,17 @@ import openpyxl
 import xlwt
 
 
-def get_equal_rows_file(source_file,source_sheet_name,target_file,target_sheet_name,targetStart,save_file):
+def get_equal_rows_file(source_file,source_sheet_name,target_file,target_sheet_name,target_start,save_file):
     source_workbook = openpyxl.load_workbook(source_file, data_only=True)
     source_sheet1 = source_workbook[source_sheet_name]
     target_workbook = openpyxl.load_workbook(target_file)
     target_sheet1 = target_workbook[target_sheet_name]
-    start = source_sheet1.max_row - 2 + 1 + targetStart
+    start = source_sheet1.max_row - 2 + 1 + target_start
     length = target_sheet1.max_row - start + 1
     target_sheet1.delete_rows(start, length)
     target_workbook.save(save_file)
 
-def copy_xlsx_to_xlsx(source_file,source_sheet,source_columns,target_file,target_sheet,target_columns,targetStart):
+def copy_xlsx_to_xlsx(source_file,source_sheet,source_columns,target_file,target_sheet,target_columns,target_start):
     source_workbook = openpyxl.load_workbook(source_file, data_only=True)
     print(source_workbook.sheetnames)
     source_worksheet1 = source_workbook[source_sheet]
@@ -26,7 +26,7 @@ def copy_xlsx_to_xlsx(source_file,source_sheet,source_columns,target_file,target
     target_sheet1 = target_workbook[target_sheet]
     for index, value in enumerate(source):
         print(value)
-        target_sheet1[f'{target_columns}' + str(index + targetStart)].value = value
+        target_sheet1[f'{target_columns}' + str(index + target_start)].value = value
     target_workbook.save(target_file)
 
 
@@ -52,6 +52,28 @@ def inventory():
     xls_workbook.save("Python-Import.xls")
 
 
+readFile1 = open("getEqualRowsInventory.txt", "r", encoding="UTF-8")
+for line1 in readFile1.readlines():
+    if "#" not in line1:
+        print(line1)
+        print(line1.split(",")[0])
+        print(line1.split(",")[1])
+        print(line1.split(",")[2])
+        print(line1.split(",")[3])
+        print(int(line1.split(",")[4]))
+        print(line1.split(",")[5])
+        get_equal_rows_file(line1.split(",")[0], line1.split(",")[1],line1.split(",")[2], line1.split(",")[3], int(line1.split(",")[4]), line1.split(",")[5].replace("\n",""))
+
+
+readFile2 = open("PInventory.txt","r",encoding="UTF-8")
+for line2 in readFile2.readlines():
+    if "#" not in line2:
+        print(line2)
+        copy_xlsx_to_xlsx(line2.split(",")[0],line2.split(",")[1],line2.split(",")[2],line2.split(",")[3],line2.split(",")[4],line2.split(",")[5],int(line2.split(",")[6]))
+
+
+inventory()
+"""  
 #库存
 get_equal_rows_file("Python-草稿-2WXX20240826.xlsx","Sheet0","库存-2WXX20240826.xlsx","ExampleSheet",3,"Python-库存-2WXX20240826.xlsx")
 copy_xlsx_to_xlsx("Python-草稿-2WXX20240826.xlsx","Sheet0",'F',"Python-库存-2WXX20240826.xlsx","ExampleSheet","A",3)
@@ -77,6 +99,6 @@ copy_xlsx_to_xlsx("Python-草稿-2WXX20240826.xlsx","Sheet0",'F',"Python-Lag-Tim
 
 inventory()
 
-
+"""
 
 
