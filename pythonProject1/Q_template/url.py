333093d6edd9cea501701bc249423a283b6caeec
Andy <Andy@qq.com>
2025-06-12 16:45:08 +0800

    稳定版

--- pythonProject1/Q_template/url.py
+++ pythonProject1/Q_template/url.py
@@ -1,14 +1,18 @@
 import time
 from urllib.error import HTTPError
 
+
 from selenium import webdriver
 from selenium.common import UnexpectedAlertPresentException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import os
 import openpyxl
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+import sys
+import pyautogui
 
 
 def fetch_amazon_data(url):
@@ -108,24 +112,63 @@ def get_img_from_row(source_workbook,source_worksheet,row):
     options.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
     chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.get("http://erpx.ksold.ltd:18085/store/product/walmart/template/add")
-    time.sleep(1)
+    #time.sleep(1)
     sku_input = chrome_driver.find_element(By.CSS_SELECTOR,".flex .el-input.el-input--default .el-input__inner")
     sku_input.send_keys(sku)
     sku_label = chrome_driver.find_element(By.CSS_SELECTOR,".w-lg-50p .el-form-item.is-required.el-form-item--default .el-form-item__label")
     sku_label.click()
+    time.sleep(2)
+    imgs = chrome_driver.find_elements(By.CSS_SELECTOR,"img")
+    # time.sleep(20)
+    chrome_driver.execute_script("window.open('');")
+    for img in imgs:
+        src_value = img.get_attribute("src")
+        if src_value.startswith("data:image"):
+            time.sleep(2)
+        else:
+            print(src_value)
+            chrome_driver.switch_to.window(chrome_driver.window_handles[1])
+            chrome_driver.get(src_value)
+
+            img_element = chrome_driver.find_element(By.CSS_SELECTOR,"img")
+            action = ActionChains(chrome_driver)
+            action.context_click(img_element).perform()
+            # time.sleep(3)
+            # plugin_option_xpath = "//div[contains(text(),'图片另存为...')]"
+            # #在AliPrice上以图搜同款
+            # plugin_option_element = chrome_driver.find_element(By.XPATH,plugin_option_xpath)
+            # plugin_option_element.click()
+            # time.sleep(3)
+            # for _ in range(7):
+            # action.send_keys(Keys.ARROW_DOWN).perform()
+            # time.sleep(3)
+            # action.send_keys(Keys.ENTER).perform()
+            pyautogui.leftClick(990,778)
+
+
+
+            break
 
 
 
 
 if __name__ == "__main__":
-    loop = True
-    while loop:
-        input1 = input("请输入行号(0退出):")
-        if int(input1) == 0:
-            loop = False
-        else:
-            print("你输入行号为:" + input1)
-            get_img_from_row(get_file(os.getcwd(),"Python-草稿"),"Sheet0", input1)
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            print(f"接收参数为:{arg}")
+            get_img_from_row(get_file(os.getcwd(), "Python-草稿"), "Sheet0", arg)
+    else:
+        print("未接收到参数")
+
+
+
+
+
+
+
+
+
+
 
 
 
